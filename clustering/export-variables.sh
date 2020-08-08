a60f8ba20563a35610be2fc41ba96cd25c5a8e36
YvesAugusto <53539227+YvesAugusto@users.noreply.github.com>
2020-08-08 14:53:03 -0300

    Create export-variables.sh

---
+++ clustering/export-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# how to run this script:
+# source <script-fullpath>
+
+
+export TRAIN_PATH=./clustering/train_features
+export TEST_PATH=./clustering/test_features
+export CLUSTER_PATH=./clustering/clusters
+
+# here you put your project path
+export PYTHONPATH=/home/yves/Downloads/clustering-redistribution
